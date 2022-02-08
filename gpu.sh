a77f17b5569594fdaebb3aa7eafe1222f5b90489
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2022-02-08 22:00:23 +0700

    Create gpu.sh

---
+++ gpu.sh
@@ -0,0 +1,8 @@
+apt install screen
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+git clone --single-branch -b verus2.2gpu https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
